620b72a81908fa33ed0e20bcba0b47067682201e
Jacob <jacobsilva2013@outlook.com>
2018-11-15 21:02:42 -0700

    Final Commit

--- References to hack and Bankrupt the terrorists/Source.cpp
+++ References to hack and Bankrupt the terrorists/Source.cpp
@@ -2,28 +2,56 @@
 
 using namespace std;
 
-void badSwap(int x, int y);
-void goodSwap(int& x, int& y);
+void badSwap(int x, int y); // Function bad swap declared with x and y integers 
+void goodSwap(int& x, int& y); // Function good swap declared with x and y integers 
 
 int main()
 {
-	int myScore = 150; // My score variable
-	int yourScore = 1000; // Your score variable
-	cout << "Original values\n";
-	cout << "myScore: " << myScore << "\n";
-	cout << "yourScore: " << yourScore << "\n\n";
-
-	cout << "Calling badSwap()\n";
-	badSwap(myScore, yourScore); // Call badSwap function
-	cout << "myScore: " << myScore << "\n";
-	cout << "yourScore: " << yourScore << "\n\n";
-
-	cout << "Calling goodSwap()\n";
-	goodSwap(myScore, yourScore); // Call goodSwap function
-	cout << "myScore: " << myScore << "\n";
-	cout << "yourScore: " << yourScore << "\n";
+	cout << "Our CIA's best coffee shop Joe's isn't doing very well... :(\n";
 	system("pause"); // System pause
+	system("cls"); // System clear
+	cout << "We at the CIA wont let Joe's coffee shop go down!\n";
+	system("pause"); // System pause
+	system("cls"); // System clear
+	cout << "We have been tracking cryptocurrency transactions being conducted by a group of terrorists!\n";
+	system("pause"); // System pause
+	system("cls"); // System clear
+	cout << "We think it's time these terrorist do something good with their money!\n";
+	system("pause"); // System pause
+	system("cls"); // System clear
+	cout << "Lets swap the funds of these terrorists to Joe's!\n";
+	system("pause"); // System pause
+	system("cls"); // System clear
 
+	int joes_funds = 0; // Joe's funds variable
+	int terrorists_funds = 100000; // Terrorists funds
+	cout << "Original account funds... \n";
+	cout << "Joe's funds " << joes_funds << "\n";
+	cout << "Terrorists funds " << terrorists_funds;
+	cout << "\n";
+	system("pause"); // System pause
+	system("cls"); // System clear
+
+	cout << "Swapping funds from terrorists to Joe's funds... \n";
+	badSwap(joes_funds, terrorists_funds); // Call badSwap function
+	cout << "Joe's funds currently " << joes_funds << "\n";
+	cout << "Terrorists funds currently " << terrorists_funds << "\n";
+	cout << "Swapping in progress...";
+	system("timeout /t 10 /nobreak"); // System timeout for 10 seconds
+	system("cls"); // System clear
+
+	cout << "Funds swapped from the terrorists funds to Joe's...\n";
+	goodSwap(joes_funds, terrorists_funds); // Call goodSwap function
+	cout << "Joe's funds " << joes_funds << "\n";
+	cout << "Terrorists funds " << terrorists_funds << "\n";
+	system("pause"); // System pause
+	system("cls"); // System clear
+
+	cout << "Good job agent we've saved Joe's Coffee and taken funds away from terrorists!\n";
+	system("pause"); // System pause
+	system("cls"); // System clear
+	cout << "It's all in a days work here at the CIA!\n";
+	system("pause"); // System pause
 	return 0;
 }
 
